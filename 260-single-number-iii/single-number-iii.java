4f809fefb371f25de254d14dd23a5ec084bed7a3
Prince kumar <princekumark132@gmail.com>
2025-09-27 01:04:09 +0530

    Time: 1 ms (99.73%) | Memory: 45.6 MB (82.16%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        
+        int  exor=0;
+        for(int i=0;i<nums.length;i++){
+            exor^=nums[i];
+        }
+        int rightmostbit=(exor&exor-1)^exor;
+        int b1=0,b2=0;
+        for(int i=0;i<nums.length;i++){
+            if((rightmostbit & nums[i])==0){
+                b1=b1^nums[i];
+            }
+            else{
+                b2^=nums[i];
+            }
+        }
+        int [] arr=new int [2];
+        arr[0]=b1;
+        arr[1]=b2;
+        
+        return arr;
+    }
+}
